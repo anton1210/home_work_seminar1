26747367870df6fcb000ff120c01ef5f77aca88f
Anton Maslov <anton-maslov3@mail.ru>
2023-02-11 21:18:47 +0300

    My home work

---
+++ max_min/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int firstNumber;
+        int secondNumber;
+        int max;
+        
+        Console.WriteLine("Введите первое число");
+        firstNumber = (int) inputValue();
+        Console.WriteLine("Введите второе число");
+        secondNumber = (int) inputValue();
+        if (firstNumber > secondNumber)
+        {
+            max = firstNumber;
+        }
+        else
+        {
+            max = secondNumber;
+        }
+        Console.WriteLine(max);
+    }
+    
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
