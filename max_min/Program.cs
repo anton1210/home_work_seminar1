17b89331bf00c73650ab85a55bb379a5b3d408b0
Anton Maslov <anton-maslov3@mail.ru>
2023-02-12 09:08:48 +0300

    Добавл min  значение

--- max_min/Program.cs
+++ max_min/Program.cs
@@ -1,6 +1,7 @@
 ﻿int firstNumber;
 int secondNumber;
 int max;
+int min;
 
 Console.WriteLine("Введите первое число");
 firstNumber = Convert.ToInt32(Console.ReadLine());
@@ -9,9 +10,12 @@ secondNumber = Convert.ToInt32(Console.ReadLine());
 if (firstNumber > secondNumber)
 {
     max = firstNumber;
+    min = secondNumber;
 }
 else
 {
     max = secondNumber;
+    min = firstNumber;
 }
 Console.WriteLine("Максимальное число: " + max);
+Console.WriteLine("Минимальное число: " + min);
