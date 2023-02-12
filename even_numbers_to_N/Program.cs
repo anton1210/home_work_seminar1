d99eec90a5643f265097c3dc6ef07a1e65ca80dc
Anton Maslov <anton-maslov3@mail.ru>
2023-02-12 11:29:25 +0300

    even numbers to N

---
+++ even_numbers_to_N/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int n;
+int i=1;
+
+Console.WriteLine("Введите число");
+n = Convert.ToInt32(Console.ReadLine());
+while (n > i)
+{
+    Console.WriteLine(i + 1);
+    i = i + 2;
+}
