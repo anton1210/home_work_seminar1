dcf933016df515903400e71dd2c0bd13bb199571
Anton Maslov <anton-maslov3@mail.ru>
2023-02-12 11:44:20 +0300

    Максимальное из 3 чисел

---
+++ max_number/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+double first;
+double second;
+double third;
+double max;
+
+Console.WriteLine("Введите первое число");
+first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+third = Convert.ToInt32(Console.ReadLine());
+if (first > second && first > third)
+{
+    max = first;
+}
+else
+{
+    if (second > third)
+    {
+        max = second;
+    }
+    else
+    {
+        max = third;
+    }
+}
+Console.WriteLine("Максимальное из введённых чисел = " + max);
